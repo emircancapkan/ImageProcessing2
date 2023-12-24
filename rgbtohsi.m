9a2c284d787c0b9e2432507972a81721cc657224
Emircan Çapkan <99869455+emircancapkan@users.noreply.github.com>
2023-12-25 00:55:08 +0300

    Update rgbtohsi.m

--- rgbtohsi.m
+++ rgbtohsi.m
@@ -1,10 +1,10 @@
-function HSI = rgbtohsi(normalizedImage)
-    R = normalizedImage(:, :, 1);
-    G = normalizedImage(:, :, 2);
-    B = normalizedImage(:, :, 3);
+function HSI = rgbtohsi(rgb_img)
+    R = rgb_img(:, :, 1);
+    G = rgb_img(:, :, 2);
+    B = rgb_img(:, :, 3);
 
     I = (R + G + B) / 3;
-    minRGB = min(normalizedImage, [], 3);
+    minRGB = min(rgb_img, [], 3);
     S = 1 - (3 * minRGB) ./ (R + G + B + eps); % Adding eps to avoid division by zero
     
     theta = acos(0.5 * ((R - G) + (R - B)) ./ sqrt((R - G).^2 + (R - B) .* (G - B) + eps)); % Adding eps to avoid division by zero
@@ -13,7 +13,7 @@ function HSI = rgbtohsi(normalizedImage)
     H = H / (2 * pi);
 
     %HSI
-    HSI=zeros(size(normalizedImage));
+    HSI=zeros(size(rgb_img));
     HSI(:,:,1)=H;
     HSI(:,:,2)=S;
     HSI(:,:,3)=I;
